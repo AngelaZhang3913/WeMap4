44e4274e98b2faed86a5096256fc2c13335af6ad
VictoriaZ04 <vzhang0779@gmail.com>
2024-02-04 03:05:37 -0600

    done!

--- app/src/main/java/com/example/wemap4/MainActivity.java
+++ app/src/main/java/com/example/wemap4/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.wemap4;
 
+import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +21,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText buildingSearch = findViewById(R.id.BuildingSearch);
         ImageButton padButton =  findViewById(R.id.padButton);
+        Button submitButton = findViewById(R.id.submitButton);
         ImageView barbieMap = findViewById(R.id.barbiemap);
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                barbieMap.setImageResource(R.drawable.barbie_map_highlight);}
+        });
+
         padButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 barbieMap.setImageResource(R.drawable.padmap);
